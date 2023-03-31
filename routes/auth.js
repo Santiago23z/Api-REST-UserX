b63ca0e050a6d5df9cbfbf6f22855e93b00ed176
Santi el papi <tiago@MacBook-Pro-de-Santi.local>
2023-03-31 12:47:50 -0500

    tasks update

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const User = require('../models/user')
 const Joi = require('@hapi/joi')
 const bcrypt = require('bcrypt')
+const moment = require("moment")
 const jwt = require('jsonwebtoken')
 
 const schemaRegister = Joi.object({
@@ -29,10 +30,30 @@ router.post('/login', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     if(!validPassword) return res.status(400).json({error: 'Constraseña invalida'})
 
+
+
+    const payload = {
+        id: user._id,
+        iat : moment(),
+        exp : moment().add(14, "days").unix()
+    }
+
+
+    function signToken (payload, token) {
+        return jwt.sign(payload, token)
+    }
+
+    const token = signToken(payload, process.env.MI_SECRET_TOKEN)
+
+    console.log(token);
+
+    
+    res.set('Authorization', `Bearer ${token}`);
     res.json({
         error: null,
-        data: 'bienvenido'
-    })
+        data: { token },
+        message: 'Bienveo'
+    });
 })
 
 
