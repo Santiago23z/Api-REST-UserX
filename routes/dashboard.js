6ef1b0a293125e35d6ea5c7ba855814931e66489
Santi el papi <tiago@MacBook-Pro-de-Santi.local>
2023-04-19 09:27:09 -0500

    fixed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -15,7 +15,7 @@ router.get('/recurso-protegido', (req, res) => {
 
 router.post("/objectsStyles", async (req, res) => {
     try {
-        const data = new syles(req.body);
+        const data = new model(req.body);
         data.usr = req.user.id;
         
         const objectSaved = await data.save();
